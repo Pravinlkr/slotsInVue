4f7c951a28de94ea17e24e29f34ddacc2a032689
Pravinlkr <pravinlakhisarai@gmail.com>
2021-08-02 16:28:54 +0530

    adding empty structure to to git in first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <c-card></c-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import cCard from './components/cCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    cCard,
   }
 }
 </script>
---
+++ src/components/cCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>card component without slots</h1>
+</template>
+<script>
+export default{
+    name:'cCard'
+}
+</script>
+<style scoped>
+
+</style>
